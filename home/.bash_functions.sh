77141b0e3e2f40521156a6dc57b5420e6d206878
Robert Smallwood <rdsmallwood928@gmail.com>
2015-08-23 21:12:30 -0600

    Updating eclim func

--- home/.bash_functions.sh
+++ home/.bash_functions.sh
@@ -85,7 +85,9 @@ updateplatform() {
 }
 
 eclim_setup() {
+  wget http://sourceforge.net/projects/eclim/files/eclim/2.5.0/eclim_2.5.0.jar
   java -Dvim.files=${HOME}/.vim -Declipse.home=${ECLIPSE_HOME} -jar eclim_2.5.0.jar install
+  rm eclim_2.5.0.jar
 }
 
 completehomeupdate() {
