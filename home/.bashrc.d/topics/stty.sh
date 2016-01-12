c29486e8e2983efcfaf55204e30902582c232efd
Robert Smallwood <rdsmallwood928@gmail.com>
2016-01-12 10:06:55 -0700

    Update stty.sh

--- home/.bashrc.d/topics/stty.sh
+++ home/.bashrc.d/topics/stty.sh
@@ -1,4 +1,15 @@
 # Use ^X instead of ^S to stop control flow so I can reclaim ^S for forward
 # common history search
-stty -f /dev/tty -ixon || stty -f /dev/tty -ixon
-stty -f /dev/tty stop ^X || stty -f /dev/tty stop ^X
+case "${PLATFORM}" in
+  darwin)
+    stty -f /dev/tty -ixon || stty -f /dev/tty -ixon
+    stty -f /dev/tty stop ^X || stty -f /dev/tty stop ^X
+  ;;
+  linux)
+    stty -F /dev/tty -ixon || stty -f /dev/tty -ixon
+    stty -F /dev/tty stop ^X || stty -f /dev/tty stop ^X  
+  ;;
+  *)
+    echo "I'm not sure how to set up stty"
+  ;;
+esac
