9514e16d133ed016e8c5ad7129dd43865dfda384
tparry1 <tparry@tendrilinc.com>
2017-05-23 14:18:18 -0600

    Update git.sh

--- home/.bashrc.d/topics/git.sh
+++ home/.bashrc.d/topics/git.sh
@@ -7,7 +7,7 @@ fi
 
 # @PERSONALIZE@
 if [[ ${hostname} =~ .+\.(ecovate|readytalk)\.com ]]; then
-  export GIT_AUTHOR_EMAIL="robert.smallwood@readytalk.com"
+  export GIT_AUTHOR_EMAIL="tparry@tendrilinc.com"
   export GIT_COMMITTER_EMAIL=${GIT_AUTHOR_EMAIL}
 fi
 
